8e3bd95eb9ee645bf332c2d2acc706e6a3855b38
Naveen Kumar <naveenkumar@Naveens-MBP.hsd1.nj.comcast.net>
2023-03-20 16:56:48 -0400

    local working version with login page

--- src/test/java/com/Utils/BaseClass.java
+++ src/test/java/com/Utils/BaseClass.java
@@ -1,11 +1,15 @@
 package com.Utils;
 
 import com.Pageobjects.Getstarted;
+import com.Pageobjects.LoginPage;
 import com.Pageobjects.RegisterPage;
 
 public class BaseClass {
 
 public 	Getstarted sp;
-public RegisterPage rp;
+public  RegisterPage rp;
+public  LoginPage lp;
+
+
 
 }
