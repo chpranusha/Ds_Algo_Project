c76a7a52898e62b56034185d5c979a50c0dbcad3
janga <mandimalap@gmail.com>
2023-03-28 21:04:13 -0400

    Added testng.xml for jenkins purpose

--- src/test/java/com/Runner/Testrunner.java
+++ src/test/java/com/Runner/Testrunner.java
@@ -9,11 +9,12 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features="src/test/resources/Features",
 					glue ={"com.Stepdefinitions","com.Utils.Helper", "appHooks"},
                  	monochrome=true,
-           // plugin= {"pretty","html:target/cucumber.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-  //   "json:target/json-report/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+            plugin= {"pretty","html:target/cucumber.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+    "json:target/json-report/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
                  	//tags = "@getstarted",
 dryRun = false)
 
 public class Testrunner {
 
 }
+
