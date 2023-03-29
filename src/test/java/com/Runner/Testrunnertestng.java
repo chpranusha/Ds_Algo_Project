e003c4a2ff9f1ecc9ec34463cd6177fa30885bda
janga <mandimalap@gmail.com>
2023-03-29 16:11:25 -0400

    pushing latest code to master branch

--- src/test/java/com/Runner/Testrunnertestng.java
+++ src/test/java/com/Runner/Testrunnertestng.java
@@ -5,14 +5,14 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/resources/Features"
 ,glue ={"com.Stepdefinitions","com.Utils.Helper", "appHooks"}
-,monochrome=true,dryRun = false ,   
+,monochrome=true,dryRun = false  
 
 
-plugin= {"pretty","html:target/cucumber.html",
-		"json:target/cucumber-reports/reports.json", 
-		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-})
+//plugin= {"pretty","html:target/cucumber.html",
+//		"json:target/cucumber-reports/reports.json", 
+//		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+//		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
 
 public class Testrunnertestng extends AbstractTestNGCucumberTests {
 	
